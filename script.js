8359cba7d38004d6cb6f549c4c63fde5d65d123d
Dydex <supremeajala@gmail.com>
2025-06-16 08:36:13 +0100

    working on question and answwer box

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const sendBtn = document.querySelector('.sendbtn');
+const textArea = document.querySelector('.text');
+
+// textArea.addEventListener('input', function () {
+//   if (textArea.value.trim() !== '') {
+//     sendBtn.classList.remove('hidden');
+//   } else {
+//     sendBtn.classList.add('hidden');
+//   }
+// });
